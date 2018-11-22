a24a667503847a9c8759ca581e5bc40e056e9649
rafal <rafal@DESKTOP-NB5L087>
2018-11-22 19:18:57 +0100

    Create project and bridge_boost_rect files.

---
+++ BridgelessBoostRectifier_v1.0/bridge_boost_rect.c
@@ -0,0 +1,18 @@
+/*
+ * bridge_boost_rect.c
+ *
+ *  Created on: 20 paŸ 2018
+ *      Author: rafal
+ */
+#include "bridge_boost_rect.h"
+
+
+inline void BoostRect_PositiveSign(BoostRect *boostRect){
+	*(boostRect->T1_pwm_register) = boostRect->pwm_max;
+	*(boostRect->T2_pwm_register) = boostRect->pwm;
+}
+
+inline void BoostRect_NegativeSign(BoostRect *boostRect){
+	*(boostRect->T1_pwm_register) = boostRect->pwm;
+	*(boostRect->T2_pwm_register) = boostRect->pwm_max;
+}
---
+++ BridgelessBoostRectifier_v1.0/bridge_boost_rect.h
@@ -0,0 +1,22 @@
+/*
+ * bridge_boost_rect.h
+ *
+ *  Created on: 20 paü 2018
+ *      Author: rafal
+ */
+#include <msp430.h>
+
+#ifndef BRIDGE_BOOST_RECT_H_
+#define BRIDGE_BOOST_RECT_H_
+
+typedef struct BoostRect{
+	unsigned int pwm;
+	unsigned int pwm_max;
+	volatile unsigned int *T1_pwm_register;
+	volatile unsigned int *T2_pwm_register;
+}BoostRect;
+
+inline void BoostRect_PositiveSign(BoostRect *boostRect);
+inline void BoostRect_NegativeSign(BoostRect *boostRect);
+
+#endif /* BRIDGE_BOOST_RECT_H_ */
